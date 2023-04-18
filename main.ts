6885e9814520f99f087f9398d14effbba2000722
417242 <115142420+417242@users.noreply.github.com>
2023-04-18 21:10:17 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,35 @@
-let 方位 = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.lightLevel())
-    if (input.lightLevel() < 100) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-    }
-    basic.pause(5000)
+    running = true
+    開始 = input.runningTime()
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    basic.showNumber(input.temperature())
+    if (!(running)) {
+        basic.showNumber(Math.idiv(時間, 1000))
+        basic.pause(100)
+        basic.showNumber(步數)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    方位 = input.compassHeading()
-    if (方位 < 45 || 方位 > 315) {
-        basic.showLeds(`
-            # # . . #
-            # # . . #
-            # . # . #
-            # . . # #
-            # . . . #
-            `)
+    if (running) {
+        時間 += input.runningTime() - 開始
     }
-    basic.pause(5000)
+    running = false
+})
+let 時間 = 0
+let 開始 = 0
+let running = false
+let 步數 = 0
+步數 = 0
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) > 1000) {
+        步數 += 1
+    }
+})
+basic.forever(function () {
+    if (running) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+    } else {
+        basic.showIcon(IconNames.Asleep)
+    }
 })
